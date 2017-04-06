da7bfdb157a054c641b02c2a157f933b08522374
parinisantiago <parinisantiago@gmail.com>
2017-04-06 19:04:30 -0300

    Passenger model

---
+++ src/main/java/model/Passenger.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.sql.Date;
+
+public class Passenger extends User {
+
+	private Float cash;
+
+	public Passenger(Float cash, Long id, String username, String pass, Date creationDate, Trip[] trips, Score[] scores) {
+		this.pass = pass;
+		this.id = id;
+		this.username = username;
+		this.creationDate = creationDate;
+		this.cash = cash;
+		this.trips = trips;
+		this.scores = scores;
+	}
+
+	public Passenger() {
+		super();
+	}
+
+	public Float getCash() {
+		return cash;
+	}
+
+	public void setCash(Float cash) {
+		this.cash = cash;
+	}
+
+	public Trip[] getTrips() {
+		return trips;
+	}
+
+	public void setTrips(Trip[] trips) {
+		this.trips = trips;
+	}
+	
+}

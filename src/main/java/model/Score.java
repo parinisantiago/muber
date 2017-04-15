f877a539a96041ed18d4ffffaa268a1e60589442
parinisantiago <parinisantiago@gmail.com>
2017-04-15 12:41:47 -0300

    modelo de puntajes

--- src/main/java/model/Score.java
+++ src/main/java/model/Score.java
@@ -4,10 +4,26 @@ public final class Score {
 	
 	private String comment;
 	private Integer score;
+	private Long oid;
 	
 	public Score() {
 		super();
 	}
+	
+	public Score(String comment, Integer score) {
+		super();
+		this.comment = comment;
+		this.score = score;
+	}
+	
+	public Long getOid() {
+		return oid;
+	}
+	
+	public void setOid(Long oid) {
+		this.oid = oid;
+	}
+	
 	
 	public String getComment() {
 		return comment;
@@ -24,4 +40,20 @@ public final class Score {
 	public void setScore(Integer score) {
 		this.score = score;
 	}
+/*
+	public Trip getTrip() {
+		return trip;
+	}
+
+	public void setTrip(Trip trip) {
+		this.trip = trip;
+	}
+
+	public Passenger getPassenger() {
+		return passenger;
+	}
+
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
+	}*/
 }

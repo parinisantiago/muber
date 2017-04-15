6150ddfca7d8e0c9a80a8c0bc2b17290709e6ef2
parinisantiago <parinisantiago@gmail.com>
2017-04-15 12:41:11 -0300

    model de pasajeros

--- src/main/java/model/Passenger.java
+++ src/main/java/model/Passenger.java
@@ -1,36 +1,60 @@
 package model;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+
 public class Passenger extends User {
 
-	private Float cash;
-	private Trip[] trips;
-    private Score[] scores;
+	private double cash;
+	private Collection<Trip> trips;
+    private Collection<Score> scores;
 
 	public Passenger() {
 		super();
 	}
+	
+	public Passenger(String username, String pass, Date creationDate, double cash) {
+		super(username, pass, creationDate);
+		this.cash = cash;
+		this.trips = new HashSet<Trip>();
+		this.scores = new HashSet<Score>();
+	}
 
-	public Float getCash() {
+
+	public double getCash() {
 		return cash;
 	}
 
-	public void setCash(Float cash) {
+	public void setCash(double cash) {
 		this.cash = cash;
 	}
 
-	public Trip[] getTrips() {
+	public Collection<Trip> getTrips() {
    		return trips;
     }
 
-    public void setTrips(Trip[] trips) {
+    public void setTrips(Collection<Trip> trips) {
    		this.trips = trips;
    	}
 
-    public Score[] getScores() {
+    public Collection<Score> getScores() {
    		return scores;
    	}
 
-    public void setScores(Score[] scores) {
+    public void setScores(Collection<Score> scores) {
    		this.scores = scores;
    	}
+    
+    public void addTrip(Trip trip){
+    	this.trips.add(trip);
+    }
+    
+    public void addScore(Score score){
+    	this.scores.add(score);
+    }
+
+	public void pay(double cost) {
+		this.cash = this.cash - cost;
+	}
 }

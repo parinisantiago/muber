4a5fc3ef00ed4c424911a41d372bacd98fdd3443
parinisantiago <parinisantiago@gmail.com>
2017-04-15 12:41:24 -0300

    modelo de conductores

--- src/main/java/model/Driver.java
+++ src/main/java/model/Driver.java
@@ -1,9 +1,20 @@
 package model;
 
+import java.util.Date;
+import java.util.Collection;
+import java.util.HashSet;
+
+
 public class Driver extends User {
 	
-	private Trip[] trips;
 	private License license;
+	private Collection<Trip> trips;
+	
+	public Driver(String username, String pass, Date creationDate, License license) {
+		super(username, pass, creationDate);
+		this.trips = new HashSet<Trip>();
+		this.license = license;
+	}
 
 	public Driver() {
 		super();
@@ -17,12 +28,16 @@ public class Driver extends User {
 		this.license = license;
 	}
 
-	public Trip[] getTrips() {
+	public Collection<Trip> getTrips() {
 		return trips;
 	}
 
-	public void setTrips(Trip[] trips) {
+	public void setTrips(Collection<Trip> trips) {
 		this.trips = trips;
+	}
+	
+	public void addTrip(Trip trip){
+		this.trips.add(trip);
 	}
 	
 }

1b6db453ff64c1d1beecdacf74f1c4a7db836973
parinisantiago <parinisantiago@gmail.com>
2017-05-03 11:45:31 -0300

    added averageScore

--- src/main/java/bd2/Muber/model/Driver.java
+++ src/main/java/bd2/Muber/model/Driver.java
@@ -3,11 +3,13 @@ package bd2.Muber.model;
 import java.util.Date;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Driver extends User {
 	
 	private License license;
 	private Collection<Trip> trips;
+	private double averageScore;
 	
 	public Driver(String username, String pass, Date creationDate, License license) {
 		super(username, pass, creationDate);
@@ -37,6 +39,24 @@ public class Driver extends User {
 	
 	public void addTrip(Trip trip){
 		this.trips.add(trip);
+	}
+
+	public double getAverageScore() {
+		return averageScore;
+	}
+
+	public void setAverageScore(double averageScore) {
+		this.averageScore = averageScore;
+	}
+	
+	public void updatesScore(){
+		int score = 0;
+		Iterator<Trip> tripIterator = this.getTrips().iterator();
+		
+		while(tripIterator.hasNext()){
+			score = score + tripIterator.next().averageScore();
+		}
+		this.setAverageScore(score / this.getTrips().size());
 	}
 	
 }

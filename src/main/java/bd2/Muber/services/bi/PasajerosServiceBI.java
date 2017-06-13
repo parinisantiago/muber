6d283f1627a70ee69e068ce299d669762e7019fa
parinisantiago <parinisantiago@gmail.com>
2017-06-13 16:10:50 -0300

    servicio de pasajeros

---
+++ src/main/java/bd2/Muber/services/bi/PasajerosServiceBI.java
@@ -0,0 +1,9 @@
+package bd2.Muber.services.bi;
+
+import java.util.Collection;
+
+import bd2.Muber.dto.PassengerDTO;
+
+public interface PasajerosServiceBI {
+	public Collection<PassengerDTO> getPasajeros();
+}

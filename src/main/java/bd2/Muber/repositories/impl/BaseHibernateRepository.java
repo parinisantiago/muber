134085f3f6c6ae3d8212b9480feab89acc30af1b
parinisantiago <parinisantiago@gmail.com>
2017-06-13 16:08:28 -0300

    repositorio padre

---
+++ src/main/java/bd2/Muber/repositories/impl/BaseHibernateRepository.java
@@ -0,0 +1,24 @@
+package bd2.Muber.repositories.impl;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+
+import bd2.Muber.repositories.bi.BaseRepositoryBI;
+
+public class BaseHibernateRepository implements BaseRepositoryBI{
+	
+	public SessionFactory sessionFactory;
+	
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+}

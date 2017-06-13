1bf0c40ad04f89483200b27c8c62b092230cf299
parinisantiago <parinisantiago@gmail.com>
2017-06-13 16:08:06 -0300

    Factory de repositorios

--- src/main/java/bd2/Muber/repositories/RepositoryLocator.java
+++ src/main/java/bd2/Muber/repositories/RepositoryLocator.java
@@ -1,5 +1,30 @@
 package bd2.Muber.repositories;
 
+import bd2.Muber.repositories.bi.PasajerosRepositoryBI;
+import bd2.Muber.repositories.impl.HibernatePasajerosRepository;
+
 public class RepositoryLocator {
+	
+	private PasajerosRepositoryBI pasajerosRepository = null;
+	private static RepositoryLocator instance = null;
+	
+	public static RepositoryLocator getInstance(){
+		if(instance == null){
+			instance = new RepositoryLocator();
+		}
+		return instance;
+	}
+	
+	public PasajerosRepositoryBI getPasajerosRepository() {
+		if(pasajerosRepository == null){
+			pasajerosRepository = new HibernatePasajerosRepository();
+		}
+		return pasajerosRepository;
+	}
+	
+	public void setPasajerosRepository(PasajerosRepositoryBI pasajerosRepository) {
+		this.pasajerosRepository = pasajerosRepository;
+	}
+	
 
 }

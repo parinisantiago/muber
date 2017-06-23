3a4129a1c22c004d665c8252fd8eccb40a100de5
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:49:13 -0300

    PasajerosRepository

--- src/main/java/bd2/Muber/repositories/impl/HibernatePasajerosRepository.java
+++ src/main/java/bd2/Muber/repositories/impl/HibernatePasajerosRepository.java
@@ -1,16 +1,22 @@
 package bd2.Muber.repositories.impl;
 
 import org.hibernate.Query;
-import org.hibernate.Session;
 
-import bd2.Muber.model.Driver;
 import bd2.Muber.model.Muber;
+import bd2.Muber.model.Passenger;
 import bd2.Muber.repositories.bi.PasajerosRepositoryBI;
 
 public class HibernatePasajerosRepository extends BaseHibernateRepository implements PasajerosRepositoryBI{
-	public  Muber getPasajeros(Session session){
-		Query query = session.createQuery("FROM Muber");
+	public  Muber getPasajeros(){
+		Query query = this.sessionFactory.getCurrentSession().createQuery("FROM Muber");
 		Muber muber = (Muber) query.list().iterator().next();
 		return muber;
+	}
+
+	@Override
+	public Passenger cargarCredito(Passenger passenger) {
+		Passenger myPassenger = (Passenger) this.sessionFactory.getCurrentSession().createQuery("FROM Passenger P WHERE id="+passenger.getId()).list().iterator().next();
+		myPassenger.addCash(passenger.getCash());
+		return myPassenger;
 	}
 }

b5b2fe0ca571380eb495131ae363c8f4c03828f8
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:47:26 -0300

    PasajerosRepository interface

--- src/main/java/bd2/Muber/repositories/bi/PasajerosRepositoryBI.java
+++ src/main/java/bd2/Muber/repositories/bi/PasajerosRepositoryBI.java
@@ -3,7 +3,10 @@ package bd2.Muber.repositories.bi;
 import org.hibernate.Session;
 
 import bd2.Muber.model.Muber;
+import bd2.Muber.model.Passenger;
 
 public interface PasajerosRepositoryBI extends BaseRepositoryBI{
-	public Muber getPasajeros(Session session);
+	public Muber getPasajeros();
+
+	public Passenger cargarCredito(Passenger passenger);
 }

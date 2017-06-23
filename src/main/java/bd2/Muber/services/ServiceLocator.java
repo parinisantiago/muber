e6cb80ccca048fc0670fd4a8213f6ba47c9f4494
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:56:16 -0300

    ServiceLocator

--- src/main/java/bd2/Muber/services/ServiceLocator.java
+++ src/main/java/bd2/Muber/services/ServiceLocator.java
@@ -1,10 +1,19 @@
 package bd2.Muber.services;
 
+import bd2.Muber.services.bi.ConductoresServiceBI;
 import bd2.Muber.services.bi.PasajerosServiceBI;
+import bd2.Muber.services.bi.PuntajesServiceBI;
+import bd2.Muber.services.bi.ViajesServiceBI;
+import bd2.Muber.services.impl.ConductoresServiceImpl;
 import bd2.Muber.services.impl.PasajerosServiceImpl;
+import bd2.Muber.services.impl.PuntajesServiceImpl;
+import bd2.Muber.services.impl.ViajesServiceImpl;
 
 public class ServiceLocator {
-	private PasajerosServiceBI pasajerosService = null;
+	private PasajerosServiceBI pasajerosService;
+	private ConductoresServiceBI conductoresService;
+	private ViajesServiceBI viajesService;
+	private PuntajesServiceBI puntajesService;
 	private static ServiceLocator instance = null;
 	public static ServiceLocator getInstance(){
 		if(instance == null){
@@ -14,13 +23,34 @@ public class ServiceLocator {
 	}
 	
 	public PasajerosServiceBI getPasajerosService(){
-		if(pasajerosService == null){
-			pasajerosService = new PasajerosServiceImpl();
-		}
 		return pasajerosService;
 	}
 
 	public void setPasajerosService(PasajerosServiceBI pasajerosService) {
 		this.pasajerosService = pasajerosService;
+	}
+
+	public ConductoresServiceBI getConductoresService() {
+		return conductoresService;	
+	}
+
+	public void setConductoresService(ConductoresServiceBI conductoresService) {
+		this.conductoresService = conductoresService;
+	}
+
+	public ViajesServiceBI getViajesService() {
+		return viajesService;	
+	}
+
+	public void setViajesService(ViajesServiceBI viajesService) {
+		this.viajesService = viajesService;
+	}
+
+	public PuntajesServiceBI getPuntajesService() {
+		return puntajesService;		
+		}
+
+	public void setPuntajesService(PuntajesServiceBI puntajesService) {
+		this.puntajesService = puntajesService;
 	}
 }

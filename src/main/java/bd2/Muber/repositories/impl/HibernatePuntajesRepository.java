fb4308b8dc6b25af27fbdde25b4abaf3c2b671e6
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:50:12 -0300

    PuntajeRepository

---
+++ src/main/java/bd2/Muber/repositories/impl/HibernatePuntajesRepository.java
@@ -0,0 +1,25 @@
+package bd2.Muber.repositories.impl;
+
+import java.util.Collection;
+
+import bd2.Muber.model.Passenger;
+import bd2.Muber.model.Score;
+import bd2.Muber.model.Trip;
+import bd2.Muber.repositories.bi.PuntajesRepositoryBI;
+
+public class HibernatePuntajesRepository extends BaseHibernateRepository implements PuntajesRepositoryBI{
+
+	@Override
+	public Score calificarViaje(Score score) {
+		@SuppressWarnings("unchecked")
+		Collection<Score> scores = this.sessionFactory.getCurrentSession().createQuery("FROM Score S, Trip T, Passenger P WHERE T.id="+score.getIdTrip()+" AND P.id="+score.getIdPassenger()).list();
+		if(!scores.isEmpty()) return null;
+		Score myScore= new Score(score.getComment(),score.getScore());
+		Trip trip = (Trip) this.sessionFactory.getCurrentSession().createQuery("FROM Trip T WHERE T.id="+score.getIdTrip()).list().iterator().next();
+		Passenger passenger=(Passenger) this.sessionFactory.getCurrentSession().createQuery("FROM Passenger P WHERE P.id="+score.getIdPassenger()).list().iterator().next();
+		trip.addScore(myScore);
+		passenger.addScore(myScore);
+		return myScore;
+	}
+
+}

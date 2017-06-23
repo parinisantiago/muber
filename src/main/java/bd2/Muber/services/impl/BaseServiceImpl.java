90add6d32efdde8ae5c8e71fe3b2103969470278
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:52:38 -0300

    BaseService

--- src/main/java/bd2/Muber/services/impl/BaseServiceImpl.java
+++ src/main/java/bd2/Muber/services/impl/BaseServiceImpl.java
@@ -2,12 +2,18 @@ package bd2.Muber.services.impl;
 
 import bd2.Muber.dto.DTOFactory;
 import bd2.Muber.repositories.RepositoryLocator;
+import bd2.Muber.repositories.bi.ConductoresRepositoryBI;
 import bd2.Muber.repositories.bi.PasajerosRepositoryBI;
+import bd2.Muber.repositories.bi.PuntajesRepositoryBI;
+import bd2.Muber.repositories.bi.ViajesRepositoryBI;
 
 public class BaseServiceImpl {
 	
 	private DTOFactory dtoFactory;
 	private PasajerosRepositoryBI pasajerosRepository = null;
+	private ConductoresRepositoryBI conductoresRepository = null;
+	private ViajesRepositoryBI viajesRepository = null;
+	private PuntajesRepositoryBI puntajesRepository = null;
 	
 	@SuppressWarnings("static-access")
 	public DTOFactory getDtoFactory(){
@@ -27,5 +33,37 @@ public class BaseServiceImpl {
 
 	public void setPasajerosRepository(PasajerosRepositoryBI pasajerosRepository) {
 		this.pasajerosRepository = pasajerosRepository;
+	}
+
+	public ConductoresRepositoryBI getConductoresRepository() {
+		if(conductoresRepository == null){
+			conductoresRepository = RepositoryLocator.getInstance().getConductoresRepository();
+		}
+		return conductoresRepository;
+	}
+
+	public void setConductoresRepository(ConductoresRepositoryBI conductoresRepository) {
+		this.conductoresRepository = conductoresRepository;
+	}
+
+	public ViajesRepositoryBI getViajesRepository() {
+		if(viajesRepository == null){
+			viajesRepository = RepositoryLocator.getInstance().getViajesRepository();
+		}
+		return viajesRepository;	
+		}
+
+	public void setViajesRepository(ViajesRepositoryBI viajesRepository) {
+		this.viajesRepository = viajesRepository;
+	}
+
+	public PuntajesRepositoryBI getPuntajesRepository() {
+		if(puntajesRepository == null){
+			puntajesRepository = RepositoryLocator.getInstance().getPuntajesRepository();
+		}
+		return puntajesRepository;		}
+
+	public void setPuntajesRepository(PuntajesRepositoryBI puntajesRepository) {
+		this.puntajesRepository = puntajesRepository;
 	}
 }

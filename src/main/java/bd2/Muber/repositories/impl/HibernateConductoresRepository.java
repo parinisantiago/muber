8f70b1550a4aa9e66376207f5d07e78bf8e4b2ad
parinisantiago <parinisantiago@gmail.com>
2017-06-23 09:33:22 -0300

    Cambio uso de metodos

--- src/main/java/bd2/Muber/repositories/impl/HibernateConductoresRepository.java
+++ src/main/java/bd2/Muber/repositories/impl/HibernateConductoresRepository.java
@@ -11,13 +11,13 @@ import bd2.Muber.repositories.bi.ConductoresRepositoryBI;
 public class HibernateConductoresRepository  extends BaseHibernateRepository implements ConductoresRepositoryBI{
 		
 		public  Muber getConductores(){
-			Query query = this.sessionFactory.getCurrentSession().createQuery("FROM Muber");
+			Query query = this.getSession().createQuery("FROM Muber");
 			Muber muber = (Muber) query.list().iterator().next();
 			return muber;
 		}
 
 		public Driver conductorDetalle(String id) {
-			Query query = this.sessionFactory.getCurrentSession().createQuery("FROM Driver D WHERE D.id ="+id);
+			Query query = this.getSession().createQuery("FROM Driver D WHERE D.id ="+id);
 			Driver driver = (Driver) query.list().iterator().next();
 			return driver;
 		}
@@ -25,7 +25,7 @@ public class HibernateConductoresRepository  extends BaseHibernateRepository imp
 		@SuppressWarnings("unchecked")
 		@Override
 		public Collection<Driver> top10Conductores() {
-			Query query = this.sessionFactory.getCurrentSession().createQuery("SELECT D FROM Driver D WHERE D.id NOT IN (SELECT T.driver.id FROM Trip T WHERE T.state = 'O') ORDER BY D.averageScore");
+			Query query = this.getSession().createQuery("SELECT D FROM Driver D WHERE D.id NOT IN (SELECT T.driver.id FROM Trip T WHERE T.state = 'O') ORDER BY D.averageScore");
 			query.setMaxResults(10);
 			return query.list();
 		}

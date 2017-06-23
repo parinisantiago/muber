04436e837a6c1e50a410e01bd398ba1d67ae793e
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:54:29 -0300

    PasajerosService

--- src/main/java/bd2/Muber/services/impl/PasajerosServiceImpl.java
+++ src/main/java/bd2/Muber/services/impl/PasajerosServiceImpl.java
@@ -9,20 +9,25 @@ import org.hibernate.Transaction;
 import bd2.Muber.dto.MuberDTO;
 import bd2.Muber.dto.PassengerDTO;
 import bd2.Muber.model.Muber;
+import bd2.Muber.model.Passenger;
 import bd2.Muber.repositories.bi.PasajerosRepositoryBI;
 import bd2.Muber.services.bi.PasajerosServiceBI;
 
 public class PasajerosServiceImpl extends BaseServiceImpl implements PasajerosServiceBI{
 	public Collection<PassengerDTO> getPasajeros(){
 		PasajerosRepositoryBI pasajerosRepo = this.getPasajerosRepository();
-		Session session = pasajerosRepo.getSession();
-		Transaction tr = session.getTransaction();
-		tr.begin();
-		MuberDTO muber = this.getDtoFactory().getMuberDTO(pasajerosRepo.getPasajeros(session));
-		@SuppressWarnings("unused")
-		int uno = 1;
-		tr.commit();
-		session.close();
+
+		MuberDTO muber = this.getDtoFactory().getMuberDTO(pasajerosRepo.getPasajeros());
+
 	    return muber.getPassengers();
+	}
+
+	@Override
+	public PassengerDTO cargarCredito(Passenger passenger) {
+		PasajerosRepositoryBI pasajerosRepo = this.getPasajerosRepository();
+
+		PassengerDTO passengetDTO = this.getDtoFactory().getPassengerDTO(pasajerosRepo.cargarCredito(passenger));
+
+	    return passengetDTO;
 	}
 }

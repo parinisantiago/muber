378a73abea196e84e343f8527a296b716654a6ea
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:51:33 -0300

    PasajerosService interface

--- src/main/java/bd2/Muber/services/bi/PasajerosServiceBI.java
+++ src/main/java/bd2/Muber/services/bi/PasajerosServiceBI.java
@@ -3,7 +3,10 @@ package bd2.Muber.services.bi;
 import java.util.Collection;
 
 import bd2.Muber.dto.PassengerDTO;
+import bd2.Muber.model.Passenger;
 
 public interface PasajerosServiceBI {
 	public Collection<PassengerDTO> getPasajeros();
+
+	public PassengerDTO cargarCredito(Passenger passenger);
 }

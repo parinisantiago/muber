d3bd82b6e77f78baeb9b0bbd5d0a5abfb2731aec
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:51:56 -0300

    PuntajeService interface

---
+++ src/main/java/bd2/Muber/services/bi/PuntajesServiceBI.java
@@ -0,0 +1,10 @@
+package bd2.Muber.services.bi;
+
+import bd2.Muber.dto.ScoreDTO;
+import bd2.Muber.model.Score;
+
+public interface PuntajesServiceBI {
+
+	ScoreDTO calificarViaje(Score score);
+
+}

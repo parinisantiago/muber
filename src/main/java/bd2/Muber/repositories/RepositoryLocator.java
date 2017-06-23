ce5ea822233fcae6f8e0b9224c6c20fe3705657e
parinisantiago <parinisantiago@gmail.com>
2017-06-23 08:50:49 -0300

    RepositoryLocator

--- src/main/java/bd2/Muber/repositories/RepositoryLocator.java
+++ src/main/java/bd2/Muber/repositories/RepositoryLocator.java
@@ -1,13 +1,18 @@
 package bd2.Muber.repositories;
 
+import bd2.Muber.repositories.bi.ConductoresRepositoryBI;
 import bd2.Muber.repositories.bi.PasajerosRepositoryBI;
-import bd2.Muber.repositories.impl.HibernatePasajerosRepository;
+import bd2.Muber.repositories.bi.PuntajesRepositoryBI;
+import bd2.Muber.repositories.bi.ViajesRepositoryBI;
 
 public class RepositoryLocator {
 	
-	private PasajerosRepositoryBI pasajerosRepository = null;
+	private PasajerosRepositoryBI pasajerosRepository;
 	private static RepositoryLocator instance = null;
-	
+	private ConductoresRepositoryBI conductoresRepository;
+	private ViajesRepositoryBI viajesRepository;
+	private PuntajesRepositoryBI puntajesRepository;
+
 	public static RepositoryLocator getInstance(){
 		if(instance == null){
 			instance = new RepositoryLocator();
@@ -15,15 +20,34 @@ public class RepositoryLocator {
 		return instance;
 	}
 	
+	public ConductoresRepositoryBI getConductoresRepository(){
+		return conductoresRepository;
+	}
+	public void setConductoresRepository(ConductoresRepositoryBI conductoresRepository) {
+		this.conductoresRepository = conductoresRepository;
+	}
 	public PasajerosRepositoryBI getPasajerosRepository() {
-		if(pasajerosRepository == null){
-			pasajerosRepository = new HibernatePasajerosRepository();
-		}
 		return pasajerosRepository;
 	}
 	
 	public void setPasajerosRepository(PasajerosRepositoryBI pasajerosRepository) {
 		this.pasajerosRepository = pasajerosRepository;
+	}
+
+	public ViajesRepositoryBI getViajesRepository() {
+		return viajesRepository;
+	}
+
+	public void setViajesRepository(ViajesRepositoryBI viajesRepository) {
+		this.viajesRepository = viajesRepository;
+	}
+
+	public PuntajesRepositoryBI getPuntajesRepository() {
+		return puntajesRepository;
+	}
+
+	public void setPuntajesRepository(PuntajesRepositoryBI puntajesRepository) {
+		this.puntajesRepository = puntajesRepository;
 	}
 	
 

b742d0840d55a47f946cd09141cfd3506059b691
parinisantiago <parinisantiago@gmail.com>
2017-06-23 09:34:05 -0300

    Cambio en uso de metodos

--- src/main/java/bd2/Muber/repositories/impl/HibernateViajesRepository.java
+++ src/main/java/bd2/Muber/repositories/impl/HibernateViajesRepository.java
@@ -16,7 +16,7 @@ public class HibernateViajesRepository extends BaseHibernateRepository implement
 
 	@Override
 	public Collection<Trip> viajesAbiertos() {
-		Query query =  this.sessionFactory.getCurrentSession().createQuery("FROM Trip T WHERE T.state = 'O' ");
+		Query query =  this.getSession().createQuery("FROM Trip T WHERE T.state = 'O' ");
 		@SuppressWarnings("unchecked")
 		Collection<Trip> trips = query.list();
 		return trips;	
@@ -25,20 +25,20 @@ public class HibernateViajesRepository extends BaseHibernateRepository implement
 	@Override
 	public Trip viajesNuevo(Trip trip) {
 		@SuppressWarnings("unchecked")
-		Collection<Driver> drivers= this.sessionFactory.getCurrentSession().createQuery("FROM Driver D, Trip T WHERE T.state='O' AND D.id ="+trip.getId()).list();
+		Collection<Driver> drivers= this.getSession().createQuery("FROM Driver D, Trip T WHERE T.state='O' AND D.id ="+trip.getId()).list();
 		if(!drivers.isEmpty()) return null;
 		
-		Driver conductor = (Driver) this.sessionFactory.getCurrentSession().createQuery("FROM Driver D WHERE D.id ="+trip.getId()).list().iterator().next();
+		Driver conductor = (Driver) this.getSession().createQuery("FROM Driver D WHERE D.id ="+trip.getId()).list().iterator().next();
 		if(conductor == null) return null;
 		Trip viaje = new Trip(trip.getOrigin(), trip.getDestiny(), trip.getCost(), trip.getMaxPassengers(), new Date(), conductor);
-		Muber muber = (Muber) this.sessionFactory.getCurrentSession().createQuery("FROM Muber").iterate().next();
+		Muber muber = (Muber) this.getSession().createQuery("FROM Muber").iterate().next();
 		muber.addTrip(viaje);
 		return viaje;
 	}
 
 	@Override
 	public Passenger agregarPasajero(Passenger passenger) {
-		Trip trip = (Trip) this.sessionFactory.getCurrentSession().createQuery("FROM Trip T WHERE T.id="+passenger.getIdTrip()).list().iterator().next();
+		Trip trip = (Trip) this.getSession().createQuery("FROM Trip T WHERE T.id="+passenger.getIdTrip()).list().iterator().next();
 		if(trip.tripIsFull()) return null;
 		if(trip.finished()) return null;
 		Passenger myPassanger = (Passenger) this.sessionFactory.getCurrentSession().createQuery("FROM Passenger P WHERE P.id="+passenger.getId()).list().iterator().next();
@@ -49,12 +49,10 @@ public class HibernateViajesRepository extends BaseHibernateRepository implement
 
 	@Override
 	public Score calificarViaje(Score score) {
-		@SuppressWarnings("unchecked")
-		Collection<Score> scores = this.sessionFactory.getCurrentSession().createQuery("FROM Score S, Trip T, Passenger P WHERE T.id="+score.getIdTrip()+" AND P.id="+score.getIdPassenger()).list();
 		Score myScore= new Score(score.getComment(),score.getScore());
-		Trip trip = (Trip) this.sessionFactory.getCurrentSession().createQuery("FROM Trip T WHERE T.id="+score.getIdTrip()).list().iterator().next();
+		Trip trip = (Trip) this.getSession().createQuery("FROM Trip T WHERE T.id="+score.getIdTrip()).list().iterator().next();
 		if(!trip.finished()) return null;
-		Passenger passenger=(Passenger) this.sessionFactory.getCurrentSession().createQuery("FROM Passenger P WHERE P.id="+score.getIdPassenger()).list().iterator().next();
+		Passenger passenger=(Passenger) this.getSession().createQuery("FROM Passenger P WHERE P.id="+score.getIdPassenger()).list().iterator().next();
 		trip.addScore(myScore);
 		passenger.addScore(myScore);
 		return myScore;
@@ -62,7 +60,7 @@ public class HibernateViajesRepository extends BaseHibernateRepository implement
 
 	@Override
 	public Trip finalizarViaje(Trip trip) {
-		Trip Mytrip = (Trip) this.sessionFactory.getCurrentSession().createQuery("FROM Trip T WHERE T.id="+trip.getId()).list().iterator().next();
+		Trip Mytrip = (Trip) this.getSession().createQuery("FROM Trip T WHERE T.id="+trip.getId()).list().iterator().next();
 		if(Mytrip.finished())return null;
 		Mytrip.finish();
 		return Mytrip;
